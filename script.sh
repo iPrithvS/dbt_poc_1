122c1f4a9b77cb5121742606f0c96639e1f63e19
iPrithvS <prithvi@squareshift.co>
2023-02-02 15:38:11 +0530

    profile and script

--- script.sh
+++ script.sh
@@ -2,3 +2,9 @@
 
 dbt --version
 dbt --version --profiles-dir .  # Pulls the most recent version of the dependencies listed in your packages.yml from git
+dbt deps --profiles-dir .  # Pulls the most recent version of the dependencies listed in your packages.yml from git
+dbt debug --target dev --profiles-dir .
+dbt debug --target prod --profiles-dir .
+dbt run --target prod --profiles-dir .
+dbt test --data --target dev --profiles-dir .
+
